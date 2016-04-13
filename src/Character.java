d16bbd2afa485d0c3877b818cb9add16abd76b7a
Jack Hamilton <jackham800@gmail.com>
2016-04-13 00:19:57 -0400

    Upload - UI

---
+++ src/Character.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author Jack
+ */
+public class Character {
+    
+    public String name = "";
+    public boolean player = false;
+    //false = male
+    public boolean gender = false;
+    public String alignment = "NN";
+    
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jack
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        List<Character> characters = new ArrayList();
+        display display = new display();
+        display.setVisible(true);
+    }
+    
+}
